6596a16e337beb9d49fbfed0648792410ea09c87
Mª Antonieta <antonieta.paixao@yahoo.com>
2022-11-11 02:45:00 -0300

    Removendo pastar com API Errada

--- PontoDePartidaAPIDotNet/src/main/java/br/com/pontodepartida/repository/CompraRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pontodepartida.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.pontodepartida.model.Compra;
-
-public interface CompraRepository extends JpaRepository<Compra, Long> {
-
-}
--- PontoDePartidaAPIDotNet/src/main/java/br/com/pontodepartida/repository/PartidaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.pontodepartida.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.pontodepartida.model.PacotePartida;
-
-public interface PartidaRepository extends JpaRepository<PacotePartida, Long> {
-
-}
